afa7a1e406d25f847964d92fbb61227a657b126e
Darykiii <110929010+Darykiii@users.noreply.github.com>
2024-04-09 16:22:53 -0300

    att

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const botoes = document.querySelectorAll(".botao");
+const text = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const botoes = document = new Date(".2024-04-25");
 
-for(let i=0; i <botoes.length;i++){
-    botoes[i].onclick = function(){
+contadores[0].textContent =tempoObjetivo1;
+
+for(let i=0; i <botoes.leosngth;i++){
+botoes[i].onclick = function(){
 
         for(let j=0; j <botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
-}
+}    
