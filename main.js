009f4e807deb949b1f7f913db01e3158ce6dba08
Darykiii <110929010+Darykiii@users.noreply.github.com>
2024-05-14 16:22:28 -0300

    atualização

--- main.js
+++ main.js
@@ -16,11 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-09-23T00:00:00");
-const tempoObjetivo2 = new Date("2024-06-23T00:00:00");
-const tempoObjetivo3 = new Date("2024-07-23T00:00:00");
-const tempoObjetivo4 = new Date("2024-08-23T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+const tempos = [tempoObjetivo1];
  
 function atualizaCronometro(){
     for (let i=0; i<contadores.length; i++){
@@ -44,6 +42,21 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
+    if(tempoFinal>0){
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else{
+    return "Prazo finalizado";
 }
+}
+function atualizaCronometro(){
+    for(let i=0; i<contadores.length;i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+    }
+
+    function comecaCronometro(){
+
+        atualizaCronometro();
+        setInterval(atualizaCronometro,1000);
+    }
+    comecaCronometro();
